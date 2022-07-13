46df7b37f0ce0ce701e391386f0d754ecb61df1b
HighnessAtharva <HighnessAtharva@gmail.com>
2022-07-13 12:10:32 +0530

    added graph and greedy post

--- delete.py
+++ delete.py
@@ -1,87 +1,59 @@
 """
-A Clique is a subgraph of graph such that all vertices in subgraph are completely connected with each other. Given a Graph, find if it can be divided into two Cliques.
+Given three stacks of the positive numbers, the task is to find the possible equal maximum sum of the stacks with the removal of top elements allowed. Stacks are represented as an array, and the first index of the array represent the top element of the stack.
 
-Examples:
+Examples: 
 
-Input : G[][] =   {{0, 1, 1, 0, 0},
-                  {1, 0, 1, 1, 0},
-                  {1, 1, 0, 0, 0},
-                  {0, 1, 0, 0, 1},
-                  {0, 0, 0, 1, 0}};
-Output : Yes
+Input : stack1[] = { 3, 10}
+  stack2[] = { 4, 5 }
+  stack3[] = { 2, 1 }
+Output : 0
+Sum can only be equal after removing all elements 
+from all stacks.
 """
+def maxSum(stack1, stack2, stack3, n1, n2, n3):
+	sum1, sum2, sum3 = 0, 0, 0
+	
+	# Finding the initial sum of stack1.
+	for i in range(n1):
+		sum1 += stack1[i]
+	
+	# Finding the initial sum of stack2.
+	for i in range(n2):
+		sum2 += stack2[i]
+	
+	# Finding the initial sum of stack3.
+	for i in range(n3):
+		sum3 += stack3[i]
 
-from queue import Queue
+# As given in question, first element is top of stack..
+	top1, top2, top3 = 0, 0, 0
+	ans = 0
+	
+	while True:
+	# If any stack is empty
+		if (top1 == n1 or top2 == n2 or top3 == n3):
+			return 0
+	# If sum of all three stack are equal.
+		if sum1 == sum2 == sum3:
+			return sum1
 
-# This function returns true if subgraph reachable from src is Bipartite or not.
-def isBipartiteUtil(G, src, colorArr):
-	global V
-	colorArr[src] = 1
-
-	# Create a queue (FIFO) of vertex numbers and enqueue source vertex for BFS traversal
-	q = Queue()
-	q.put(src)
-
-	# Run while there are vertices in queue (Similar to BFS)
-	while (not q.empty()):
-		
-		# Dequeue a vertex from queue
-		u = q.get()
-
-		# Find all non-colored adjacent vertices
-		for v in range(V):
-			
-			# An edge from u to v exists and destination v is not colored
-			if (G[u][v] and colorArr[v] == -1):
-				
-				# Assign alternate color to this adjacent v of u
-				colorArr[v] = 1 - colorArr[u]
-				q.put(v)
-
-			# An edge from u to v exists and destination v is colored with same color as u
-			elif (G[u][v] and colorArr[v] == colorArr[u]):
-				return False
-
-	# If we reach here, then all adjacent vertices can be colored with alternate color
-	return True
-
-# Returns true if a Graph G[][] is Bipartite or Note that G may not be connected.
-def isBipartite(G):
-	global V
-	# Create a color array to store colors assigned to all vertices. Vertex number is used as index in this array. The value '-1' of colorArr[i] is used to indicate that no color is assigned to vertex 'i'. The value 1 is used to indicate first color is assigned and value 0 indicates second color is assigned.
-	colorArr = [-1] * V
-
-	# One by one check all not yet colored vertices.
-	for i in range(V):
-		if (colorArr[i] == -1):
-			if (isBipartiteUtil(G, i, colorArr) == False):
-				return False
-
-	return True
-
-# Returns true if G can be divided into
-# two Cliques, else false.
-def canBeDividedinTwoCliques(G):
-	global V
-	# Find complement of G[][] All values are complemented except diagonal ones
-	GC = [[None] * V for _ in range(V)]
-	for i in range(V):
-		for j in range(V):
-			GC[i][j] = not G[i][j] if i != j else 0
-
-	# Return true if complement is Bipartite else false.
-	return isBipartite(GC)
+	# Finding the stack with maximum sum and removing its top element.
+		if (sum1 >= sum2 and sum1 >= sum3):
+			sum1 -= stack1[top1]
+			top1=top1+1
+		elif (sum2 >= sum1 and sum2 >= sum3):
+			sum2 -= stack2[top2]
+			top2=top2+1
+		elif (sum3 >= sum2 and sum3 >= sum1):
+			sum3 -= stack3[top3]
+			top3=top3+1
 
 
-V = 5
-G = [[0, 1, 1, 1, 0],
-	[1, 0, 1, 0, 0],
-	[1, 1, 0, 0, 0],
-	[0, 1, 0, 0, 1],
-	[0, 0, 0, 1, 0]]
-if canBeDividedinTwoCliques(G):
-	print("Yes")
-else:
-	print("No")
-
+stack1 = [ 3, 2, 1, 1, 1 ]
+stack2 = [ 4, 3, 2 ]
+stack3 = [ 1, 1, 4, 1 ]
+n1 = len(stack1)
+n2 = len(stack2)
+n3 = len(stack3)
+print (maxSum(stack1, stack2, stack3, n1, n2, n3))
 
